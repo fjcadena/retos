bf93da70d8a81785d0e9bd4c6c575b144127a790
fjcadena <126709126+fjcadena@users.noreply.github.com>
2024-10-30 19:52:40 +0100

    Update main.c
    Actualizar des linux

--- main.c
+++ main.c
@@ -1,49 +1,44 @@
+// Created by javiercadena on 29/10/24.
+//
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Nodo {
-    int dato;
+    int valor;
     struct Nodo *before;
     struct Nodo *next;
 };
 
-struct Nodo* insertar_nodo(struct Nodo *actual, struct Nodo *priner_nodo, int valor, size_t size_nodo);
-struct Nodo* find_nodo(struct Nodo* primer_elemento, int buscar);
+struct Nodo* insertar(struct Nodo *actual, struct Nodo *primer_nodo, int dato, size_t size_nodo);
+struct Nodo* buscar(struct Nodo *primer_nodo, int buscar);
+void borrar(struct Nodo *ptr_nodo, struct Nodo *primer_nodo);
 
-void delete_nodo(void);
-
-
-struct Nodo* insertar_nodo(struct Nodo *actual, struct Nodo *primer_nodo, int valor, size_t size_nodo){
-    //posicion
-    if (primer_nodo == NULL){
-        primer_nodo = (struct Nodo*)malloc(size_nodo);
+struct Nodo* insertar(struct Nodo *actual, struct Nodo *primer_nodo, int dato, size_t size_nodo){
+    if (primer_nodo == NULL) {
+        primer_nodo = (struct Nodo*) malloc(size_nodo);
         primer_nodo->before = NULL;
         primer_nodo->next = NULL;
-        primer_nodo->dato = valor;
+        primer_nodo->valor = dato;
         actual = primer_nodo;
     }
-    else {
-        struct Nodo *prt_nodo = (struct Nodo *) malloc(size_nodo);
-
-        prt_nodo = (struct Nodo *) malloc(size_nodo);
-        prt_nodo->next = actual->next;
-        actual->next = prt_nodo;
-        prt_nodo->before = actual;
-        prt_nodo->dato = valor;
-        actual = prt_nodo;
+    else{
+        struct Nodo *ptr_new = malloc(size_nodo);
+        ptr_new->next = actual->next;
+        if (actual->next != NULL){  //si no es el ultimo el siguiente apunta al nuevo
+            actual->next->before = ptr_new;
+        }
+        actual->next = ptr_new;
+        ptr_new->before = actual;
+        ptr_new->valor = dato;
+        actual=ptr_new;
     }
     return primer_nodo;
 }
 
-
-struct Nodo* find_nodo(struct Nodo* primer_elemento, int buscar){
-    struct Nodo* ptr_nodo = primer_elemento;
-    // aqui tenemos dos opciones o mirar hasta que el valor de next sea NULL y luego añadir un if al final
-    // para verificar si el ultimo registro es correcto mirar si ptr es NULL y asi eliminamos esa verificación
-
+struct Nodo* buscar(struct Nodo *primer_nodo, int buscar){
+    struct Nodo* ptr_nodo = primer_nodo;
     while (ptr_nodo != NULL){
-        if (ptr_nodo->dato == buscar){
+        if (ptr_nodo->valor == buscar){
             return ptr_nodo;
         }
         ptr_nodo = ptr_nodo->next;
@@ -51,16 +46,28 @@ struct Nodo* find_nodo(struct Nodo* primer_elemento, int buscar){
     return NULL;
 }
 
+void borrar(struct Nodo *ptr_nodo, struct Nodo *primer_nodo){
+    if (ptr_nodo == primer_nodo)
+        primer_nodo = ptr_nodo->next;
+    if (ptr_nodo->before != NULL)
+        ptr_nodo->before->next = ptr_nodo->next;
+    if (ptr_nodo->next != NULL)
+        ptr_nodo->next->before = ptr_nodo->before;
 
+    free(ptr_nodo);
+    ptr_nodo = NULL;
+}
 
-int main() {
-    size_t size_struct = sizeof(struct Nodo);
-    struct Nodo *primero = NULL;
-    struct Nodo *actual = NULL;
-    int dato;
+int main(void){
+    size_t size_nodo = sizeof(struct Nodo);
 
-    primero = insertar_nodo(actual, primero, dato, size_struct);
+    //declaramos variables a estructura
+    //struct Nodo *posicion_actual = (struct Nodo*)malloc(sizeof(struct Nodo));
+    struct Nodo *actual = malloc(size_nodo);
+    struct Nodo *primer_nodo = NULL;
+    int valor = 5;
+    //insertar(posicion_actual,primer_nodo,valor,size_nodo);
 
-    printf("Hello, World!\n");
+    printf("hola mundo");
     return 0;
 }
